7c3ca7da2a4484c241e6907b57532aa30b1ae3ce
konstantin0s <constantintofan85@gmail.com>
2020-04-08 19:30:33 +0200

    Add react js

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const hbs = require('hbs');
 
 var app = express();
 
+//enables cors
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+
+
+// app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+
+var breweries = require('./routes/breweries');
+  app.use('/', breweries);
 
   // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/breweries.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+require('dotenv').config();
+
+const apiUrl = process.env.API_URL;
+const apiKey = process.env.API_KEY;
+
+router.get('/', function (req, res, next) {
+    res.render('locations');
+});
+
+
+//get all breweries by name
+
+router.get('/breweries', function (req, res, next) {
+ 
+  const options = {
+    params: {
+      key: apiKey,
+      name: req.query.name || '',
+      p: req.query.page || 1
+    },
+    withCredentials: true
+  };
+
+  axios.get(`${apiUrl}/breweries`, options).then(function (response) {
+    // res.send(response.data);
+    var body = response.data;
+    res.json(body);
+    // console.log(body)
+    // res.render('locations', {body: body})
+  })
+});
+
+
+//Search beers by id
+router.get('/brewery/:id', function (req, res, next) {
+  const options = {
+    params: {
+      key: apiKey
+    },
+    withCredentials: true
+  }
+  axios.get(`${apiUrl}/brewery/${req.params.id}`, options).then(function (response) {
+      // res.send(response.data);
+      // var body = response.data;
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      res.status(404).send();
+    });
+});
+
+//get all breweries by location
+
+router.get('/locations', function (req, res, next) {
+ 
+  const options = {
+    params: {
+      key: apiKey,
+      name: req.query.name || '',
+      p: req.query.page || 1
+    },
+    withCredentials: true
+  };
+
+  axios.get(`${apiUrl}/locations`, options).then(function (response) {
+    // res.send(response.data);
+    var body = response.data;
+    res.json(body);
+    // console.log(body)
+    // res.render('locations', {body: body})
+  })
+});
+
+
+
+
+
+//Search brewery by id and location
+// router.get('/brewery/:id', function (req, res, next) {
+//   const options = {
+//     params: {
+//       key: apiKey
+//     },
+//       withCredentials: true
+//   }
+
+//   axios.get(`${apiUrl}/brewery/${req.params.id}/locations`, options)
+//   .then(function (response) {
+//       // res.send(response.data);
+//       console.log(response);
+//       console.log(req.params.id);
+//       var body = response.data.data;
+//       res.json(body);
+//       console.log(body);
+//     })
+//     .catch(function (error) {
+//       res.status(404).send();
+//     });
+// });
+
+module.exports = router;
